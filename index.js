9d101cce785b8fa918563a2ec6ae82b96c32b981
Owen Pierce <opierce@gmail.com>
2024-09-21 14:00:34 -0500

    Adding custom objects list link

--- index.js
+++ index.js
@@ -8,19 +8,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN; //'';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+function requestHeaders() {
+    return {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': "application/json"
+    };
+}
+
+app.get('/', async (req, res) => {
+    // render the home page
+    res.render('contacts');
+});
+
+app.get('/contacts', async (req, res) => {
+    const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/contact_form', async (req, res) => {
+
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update', async (req, res) => {
+
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
