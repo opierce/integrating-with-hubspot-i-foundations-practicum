544db0cea9ab5bd907ebffbabac8ecd61822fcf7
Owen Pierce <opierce@gmail.com>
2024-09-27 19:40:57 -0700

    We can add cats, redirect home, and some table styling

--- index.js
+++ index.js
@@ -16,23 +16,38 @@ const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN; //'';
 function requestHeaders() {
     return {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': "application/json"
+        'Content-Type': 'application/json'
     };
 }
 
 app.get('/', async (req, res) => {
     // render the home page
-    res.render('homepage');
+    const cats = 'https://api.hubapi.com/crm/v3/objects/cats?idProperty=name&properties=name,bio,cutness_level,preferred_food';
+    const headers = requestHeaders();
+
+    try {
+        const resp = await axios.get(cats, { headers });
+        const data = resp.data.results;
+        // console.log(data);
+
+        res.render('homepage', { 'title': 'Homepage | Integrating With HubSpot I Practicum', data });
+
+    } catch (err) {
+        console.error(err);
+        console.log(requestHeaders());
+    }
 });
 
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
     const headers = requestHeaders();
+
     try {
         const resp = await axios.get(contacts, { headers });
         const data = resp.data.results;
 
-        res.render("contacts", { "title": "Contacts | Integrating With HubSpot I Practicum", data });
+        res.render('contacts', { 'title': 'Contacts | Integrating With HubSpot I Practicum', data });
+
     } catch (err) {
         console.error(err);
         console.log(requestHeaders());
@@ -42,15 +57,51 @@ app.get('/contacts', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
-    const headers = requestHeaders();
+    res.render('updates', { 'title': 'Add a Cat | Integrating With HubSpot I Practicum' });
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 app.post('/update-cobj', async (req, res) => {
-    const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
+    const contacts = 'https://api.hubapi.com/crm/v3/objects/cats';
     const headers = requestHeaders();
+
+    var dryFood = req.body.dry_food;
+    var wetFood = req.body.wet_food;
+
+    const dryFoodLiteral = 'Dry Food';
+    const wetFoodLiteral = 'Wet Food';
+
+    var preferred_food = '';
+    if (dryFood && wetFood) {
+        preferred_food = dryFoodLiteral + ";" + wetFoodLiteral;
+
+    } else if (dryFood) {
+        preferred_food = dryFoodLiteral;
+
+    } else if (wetFood) {
+        preferred_food = wetFoodLiteral;
+    }
+
+    const catData = {
+        'properties': {
+            'name': req.body.name,
+            'bio': req.body.bio,
+            'cutness_level': req.body.cuteness_level,
+            'preferred_food': preferred_food
+        }
+    };
+
+    try {
+        const resp = await axios.post(contacts, catData, { headers });
+        const data = resp.data.results;
+
+        res.redirect('/');
+
+    } catch (err) {
+        console.log('UPDATE WENT WRONG');
+        console.error(err);
+    }
 });
 
 /** 
@@ -76,7 +127,7 @@ app.get('/contacts', async (req, res) => {
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            'favorite_book': req.body.newVal
         }
     }
 
